01f9802f19fe4bcb6feb9e9cdfcba687129636df
siyuanxu <i@siyuanxu.com>
2018-01-05 19:17:12 +0800

    update code

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ with codecs.open('index.md', 'w', 'utf-8') as index:
     weather_2 = weather('漯河', config).to_write
     weather_3 = weather('泸州', config).to_write
 
-    index.write('## {0} 天气')
+    index.write('## {0} 天气'.format(weather_main.date))
     index.write(weather_1)
     index.write(weather_2)
     index.write(weather_3)  # use coding utf8
