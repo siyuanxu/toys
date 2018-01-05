09fd27803d49d78a62927af1a5ed5ec2560f977b
siyuanxu <i@siyuanxu.com>
2018-01-05 11:25:43 +0800

    update code

--- code_update.sh
+++ code_update.sh
@@ -1,4 +1,3 @@
-iconv -t UTF-8 index_.md > index.md
 git add .
 git commit -m "update code"
 git push
--- main.py
+++ main.py
@@ -1,16 +1,17 @@
-#-*-coding:utf-8-*-
+# -*-coding:utf-8-*-
 from weather_api import weather
 import yaml
+import codecs
 
-with open('index.md', 'w','utf-8') as index:
+with codecs.open('index.md', 'w', 'utf-8') as index:
     config = yaml.load(open('config.yaml'))
 
-    weather_main=weather('南京', config)
+    weather_main = weather('南京', config)
     weather_1 = weather_main.to_write
     weather_2 = weather('漯河', config).to_write
     weather_3 = weather('泸州', config).to_write
 
-    index.write('## {0} 天气'.format(weather_main.date))
+    index.write('## {0} 天气')
     index.write(weather_1)
     index.write(weather_2)
-    index.write(weather_3)
+    index.write(weather_3)  # use coding utf8
