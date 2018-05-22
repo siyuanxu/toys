fd0f9e1a529ab075219821f71b61c8668da1ef11
siyuanxu <i@siyuanxu.com>
2018-05-22 11:55:03 +0800

    set new interval

--- main.py
+++ main.py
@@ -16,31 +16,52 @@ config = yaml.load(open('config.yaml', encoding="utf8"))
 # if not, keep the old tmp and do nothing
 
 
-market_time = [928, 1202, 1328, 1502]
-time_now = int(time.strftime('%H%M', time.localtime(time.time())))
+def stock_toy(config):
+    market_time = [928, 1202, 1328, 1502]
+    time_now = int(time.strftime('%H%M', time.localtime(time.time())))
 
-if time_now in range(market_time[0],
-                     market_time[1]) or time_now in range(market_time[2],
-                                                          market_time[3]):
-    with codecs.open('finace_tmp.txt', 'w', 'utf-8') as ft:
-        # fund toy
-        fund_res = fund(config).to_write
-        ft.write(fund_res)
+    if time_now in range(market_time[0],
+                         market_time[1]) or time_now in range(market_time[2],
+                                                              market_time[3]):
+        with codecs.open('finace_tmp.txt', 'w', 'utf-8') as ft:
+            # fund toy
+            fund_res = fund(config).to_write
+            ft.write(fund_res)
 
-        # vip stock toy
-        vip_stock = stock_pool(config)
-        ft.write(vip_stock.to_write)
+            # vip stock toy
+            vip_stock = stock_pool(config)
+            ft.write(vip_stock.to_write)
 
-else:
-    pass
-with codecs.open('finace_tmp.txt', 'r', 'utf-8') as ft:
-    ft_content = ''.join(ft.readlines())
+    else:
+        pass
+    with codecs.open('finace_tmp.txt', 'r', 'utf-8') as ft:
+        ft_content = ''.join(ft.readlines())
+    return ft_content
 
+# xmr mining toy
+
+
+def xmr_toy():
+    time_now = int(time.strftime('%H%M', time.localtime(time.time())))
+    hour_now = int(time.strftime('%H', time.localtime(time.time())))
+    if (hour_now * 100 - 5) < time_now < (hour_now * 100 + 5):
+        xmr_info = xmr()
+        with codecs.open('xmr_tmp.txt', 'w', 'utf-8') as xmr_temp:
+            xmr_temp.write(xmr_info.str)
+    else:
+        pass
+    
+    with codecs.open('xmr_tmp.txt', 'r', 'utf-8') as xt:
+        xt_content = ''.join(xt.readlines())
+    return xt_content    
+
+
+ft_content=stock_toy(config)
+xt_content = xmr_toy()
 
 with codecs.open('index.md', 'w', 'utf-8') as index:
-  
-    xmr_info = xmr()
-    index.write(xmr_info.str)
+
+    index.write(xt_content)
 
     index.write(ft_content)
 
